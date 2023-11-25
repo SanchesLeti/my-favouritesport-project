189de4044c895e4020214520d303dbfc1b27f891
SanchesLeti <145586269+SanchesLeti@users.noreply.github.com>
2023-11-25 18:07:44 +0000

    Removed java file and implement js in html

--- java.js
+++
@@ -1,8 +0,0 @@
-function changeTheme() {
-    let body = document.querySelector('body');
-    body.classList.add("dark-theme");
-}
-
-let themeButtonn = document.querySelector("button");
-button.addEventListener("click, changeTheme);
-")
