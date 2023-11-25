cc117691b3802303a0389a410d5df5f6d1f51dab
SanchesLeti <145586269+SanchesLeti@users.noreply.github.com>
2023-11-25 17:26:22 +0000

    add dark theme style and linked js to html

---
+++ java.js
@@ -0,0 +1,8 @@
+function changeTheme() {
+    let body = document.querySelector('body');
+    body.classList.add("dark-theme");
+}
+
+let themeButtonn = document.querySelector("button");
+button.addEventListener("click, changeTheme);
+")
